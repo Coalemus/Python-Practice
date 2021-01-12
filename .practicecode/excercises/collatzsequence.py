d8851f376c90e4ef94db9320f680ee825e9797ea
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-01-13 05:30:23 +0800

    code not working but code nonetheless

--- .practicecode/excercises/collatzsequence.py
+++ .practicecode/excercises/collatzsequence.py
@@ -1,14 +1,14 @@
 def collatz(number):
     while number > 1:
         if number % 2 == 0:
-            evenres = print(number // 2)
+            return number // 2
         elif number % 2 == 1:
-            oddres = print( 3 * number + 1)
+            return 3 * number + 1
+
 
 data = int(input("Enter number: "))
 
-
-
-collatz(data)
-
-
+try:
+    collatz(data)
+except ValueError as er:
+    print("Please enter an integer.")
