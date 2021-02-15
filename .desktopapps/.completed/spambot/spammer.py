1d2ec24e577ba88570d34a857d449900e87bbf39
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-02-16 02:14:22 +0800

    optimized code

--- .desktopapps/.completed/spambot/spammer.py
+++ .desktopapps/.completed/spambot/spammer.py
@@ -1,6 +1,6 @@
 import pyautogui, time
 time.sleep(5)
-f = open("spamtext.txt", "r")
+f = open("C:\\Users\\Flores\\Joey-vscode-workspaces\\General-Python-Repository\\.desktopapps\\.completed\\spambot\\spamtext.txt", "r")
 for word in f:
     pyautogui.typewrite(word)
     pyautogui.press("enter")
