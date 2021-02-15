1ab2d51a79bee3b7d2a5ab905e0b45320b20834c
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-02-16 03:05:36 +0800

    Changed folder name

--- automatewithpython/snippets/fileopen.py
+++
@@ -1,4 +0,0 @@
-hellofile = open('C:\\Users\\Flores\\Joey-vscode-workspaces\\General-Python-Repository\\automatewithpython\\practicetxts\\helloworld.txt')
-filecontent = hellofile.read()
-
-print(filecontent)
