51dfd07782802d58c002ed274c8b7a5a88af7db0
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-02-26 02:27:47 +0800

    moved Projects directory to its own repo

--- .projects/.completed/spambot/spammer.py
+++
@@ -1,9 +0,0 @@
-#! python3
-import pyautogui, time
-time.sleep(5)
-f = open("C:\\Users\\Flores\\Joey-vscode-workspaces\\General-Python-Repository\\.desktopapps\\.completed\\spambot\\navyspamtext.txt", "r")
-for word in f:
-    pyautogui.typewrite(word)
-    pyautogui.press("enter")
-
-
--- .projects/.inprogress/minutesvisualizer/minutesvisualizer.py
+++
@@ -1,4 +0,0 @@
-# code an application that visualizes the key events made at a meeting; 
-# code a database for minutes data storage;
-# code a ui for ease of access
-# code a gui for visualization of minutes of a meeting
